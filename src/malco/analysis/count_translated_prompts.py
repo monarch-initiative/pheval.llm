91cb283d3724e309521f61858a580f0c67524e1d
Leonardo macOS <leonardochimirri94@gmail.com>
2024-11-26 11:10:41 +0100

    added babelon table creation, also fixed creation of prompts wo missing translations in any language, added a dropna to make sure #52 is solved

--- src/malco/analysis/count_translated_prompts.py
+++ src/malco/analysis/count_translated_prompts.py
@@ -1,5 +1,7 @@
+import shutil
 import os
 import re
+import glob
 
 fp = "/Users/leonardo/IdeaProjects/phenopacket2prompt/prompts/"
 
@@ -11,6 +13,7 @@ langs = [
     "nl",
     "tr",
     "zh",
+    "cs",
 ]
 
 promptfiles = {}
@@ -18,9 +21,10 @@ for lang in langs:
     promptfiles[lang] = []
     for dirpath, dirnames, filenames in os.walk(fp + lang):
         for fn in filenames:
-            fn = fn[0:-14]  # TODO may be problematic if there are 2 "_" before "{langcode}-"
+            #fn = fn[0:-14]  # TODO may be problematic if there are 2 "_" before "{langcode}-"
             # Maybe something along the lines of other script disease_avail_knowledge.py
             # ppkt_label = ppkt[0].replace('_en-prompt.txt','')
+            fn = fn.replace('_' + lang +'-prompt.txt','')
             promptfiles[lang].append(fn)
         break
 
@@ -33,7 +37,18 @@ itset = set(promptfiles["it"])
 nlset = set(promptfiles["nl"])
 zhset = set(promptfiles["zh"])
 trset = set(promptfiles["tr"])
+csset = set(promptfiles["cs"])
 
-intersection = enset & esset & deset & itset & nlset & zhset & trset
+intersection = enset & esset & deset & itset & nlset & zhset & trset & csset
 
 print("Common ppkts are: ", len(intersection))
+
+# COPY
+dst_dir = "/Users/leonardo/git/malco/in_multlingual_nov24/prompts/"
+for id in intersection:
+    for lang in langs:
+        shutil.copy(fp + lang + "/" + id + '_' + lang +'-prompt.txt', dst_dir + lang) 
+
+#        file = glob.glob(fp + lang + "/" + id + "*")
+#        for match in file:
+#            shutil.copy(match, dst_dir + lang ) 

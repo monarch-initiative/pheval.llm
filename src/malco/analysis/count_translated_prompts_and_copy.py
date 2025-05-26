25bb8908a64af25b50a8d2a2cea350d8046f4ec8
Leonardo macOS <leonardochimirri94@gmail.com>
2025-05-26 16:45:45 +0200

    corrected copy ppkts function, adding feature of copying jsons, update notebook for stats with results of both meditron and gpt

--- src/malco/analysis/count_translated_prompts_and_copy.py
+++ src/malco/analysis/count_translated_prompts_and_copy.py
@@ -1,24 +1,46 @@
-"""Look in the phenopacket2prompt output directory for the common phenopackets across languages and copy them to an input directory."""
+"""Look in the phenopacket2prompt output directory for the common phenopackets across languages and copy them to another directory.
+"""
 
 import os
 import re
 import shutil
 import tqdm
+import sys
+import json
 
-fp = "/Users/leonardo/IdeaProjects/phenopacket2prompt/6668prompts/"
 
-langs = [
-    "en",
-    "ja",
-    "es",
-    "de",
-    "it",
-    "nl",
-    "tr",
-    "zh",
-    "cs",
-    "fr",
-]
+create_list_file = False
+copy_prompt_files = False
+copy_json_files = True
+
+try:
+    fp = sys.argv[1]
+except IndexError:
+    print("No path provided, using default path.")
+    # Default path to the phenopacket2prompt output directory
+    fp = "/Users/leonardo/IdeaProjects/phenopacket2prompt/6668prompts/"
+
+try:
+    output_file = sys.argv[2]
+except IndexError:
+    print("No output file provided, using default output file.")
+    # Default output file to save the common phenopackets
+    output_file = "final_multilingual_output/ppkts_4917set.txt"  # Specify the output file name
+
+try:
+    dst_dir = sys.argv[3]
+except IndexError:
+    # Default destination directory to copy the files
+    print("No destination directory provided, using default destination directory.")
+    dst_dir = "/Users/leonardo/data/4917_poly_ppkts"
+
+# Take as a second argument the list of languages to consider
+if len(sys.argv) > 4:
+    langs = sys.argv[4].split(",")
+else:
+    # Default languages to consider
+    langs = ["en", "ja", "es", "de", "it", "nl", "tr", "zh", "cs", "fr"]
+    print("No languages provided, using default languages: ", langs, "\nYou can provide them as a comma-separated list as the second argument.")
 
 promptfiles = {}
 for lang in langs:
@@ -31,28 +53,39 @@ for lang in langs:
 
 intersection = set()
 
-enset = set(promptfiles["en"])
-esset = set(promptfiles["es"])
-deset = set(promptfiles["de"])
-itset = set(promptfiles["it"])
-nlset = set(promptfiles["nl"])
-zhset = set(promptfiles["zh"])
-trset = set(promptfiles["tr"])
-jaset = set(promptfiles["ja"])
-csset = set(promptfiles["cs"])
-frset = set(promptfiles["fr"])
+# Convert lists to sets for intersection
+promptfiles = {lang: set(files) for lang, files in promptfiles.items()}
 
-intersection = enset & esset & deset & itset  & zhset & trset & csset & jaset  & nlset & frset
+# Create an intersection set of all languages
+# Initialize the intersection with the first language's set
+if langs:
+    intersection = promptfiles[langs[0]]
+# Intersect with the sets of the other languages
+for lang in langs[1:]:
+    intersection &= promptfiles[lang]
 
 print("Common ppkts are: ", len(intersection))
 
-output_file = "final_multilingual_output/ppkts_4917set.txt"  # Specify the output file name
-with open(output_file, "w") as f:
-    for item in intersection:
-        f.write(item + "_en-prompt.txt\n")  # Write each item followed by a newline character
+if create_list_file:
+    with open(output_file, "w") as f:
+        for item in intersection:
+            f.write(item + "_en-prompt.txt\n")  # Write each item followed by a newline character
 
-"""# COPY
-dst_dir = "/Users/leonardo/git/malco/in_multlingual_nov24/prompts/"
-for id in tqdm.tqdm(intersection, "Copying files..."):
-    for lang in langs:
-        shutil.copy(fp + lang + "/" + id + "_" + lang + "-prompt.txt", dst_dir + lang)"""
+# Copy prompts
+if copy_prompt_files:
+    for id in tqdm.tqdm(intersection, "Copying files..."):
+        for lang in langs:
+            shutil.copy(fp + lang + "/" + id + "_" + lang + "-prompt.txt", dst_dir + lang)
+
+# Copy jsons
+if copy_json_files:
+    json_path = os.path.join(fp, "original_phenopackets")
+    for jsonfile in tqdm.tqdm(os.listdir(json_path), "Copying json files..."):
+        with open(os.path.join(json_path, jsonfile), 'r') as f:
+            data = json.load(f)
+            id = data['id']
+            id = re.sub(r'[^\w]', '_', id)
+            if id in intersection:
+                shutil.copy(os.path.join(json_path, jsonfile), os.path.join(dst_dir, "jsons", jsonfile))
+            else:
+                print(f"Skipping {jsonfile}, not in intersection.")
